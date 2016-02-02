2030f92a6d9c46dbc61d52bbd8666becb4a80ba1
wassertim <tim@digitalband.ru>
2016-02-02 16:19:23 +0100

    delete some files

--- interfaces/hybrid-interfaces.js
+++
@@ -1,4 +0,0 @@
-var c;
-c(10);
-c.reset();
-c.interval = 5.0;
--- interfaces/object-shape.js
+++
@@ -1,7 +0,0 @@
-var berlin = {};
-berlin.title = 'Berlin';
-berlin.population = 3502000;
-var moscow = {
-    title: 'Moscow',
-    population: 11920000
-};
