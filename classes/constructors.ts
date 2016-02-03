7a30791357ff1c1b2c4328e17d43f453082e609d
wassertim <tim@digitalband.ru>
2016-02-03 09:10:25 +0100

    add some files

---
+++ classes/constructors.ts
@@ -0,0 +1,12 @@
+class Greeter {
+    static standardGreeting = "Hello, there";
+    greeting: string;
+    greet() {
+        if (this.greeting) {
+            return "Hello, " + this.greeting;
+        }
+        else {
+            return Greeter.standardGreeting;
+        }
+    }
+}
